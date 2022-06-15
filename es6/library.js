ee3c32331751b1183b867b63ccbc91f035ce583a
Envigo <rayeesh.alam@envigo.co.uk>
2022-06-15 21:35:10 +0530

    second code

---
+++ es6/library.js
@@ -0,0 +1,8 @@
+let sum = (a, b) => a + b;
+let minus = (a, b) => a - b;
+let multiplication = (a, b) => a * b;
+let divition = (a, b) => a / b;
+let modules = (a, b) => a % b;
+
+
+export { sum, minus, multiplication as multi, divition, modules };
